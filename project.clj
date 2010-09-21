71d3ff6245993276bed081c6004948d05bc34722
Raynes <DiscipleRayne@gmail.com>
2010-09-20 19:38:29 -0500

    Should actually work now.

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject cake-autodoc "0.0.1-SNAPSHOT"
   :description "A task for generating autodoc documentation."
   :dependencies [[clojure "1.2.0"]
-                 [clojure-contrib "1.2.0"]
                  [org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]]
-  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]
-                     [clojure-contrib "1.2.0"]]
+  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]]
   :tasks [cake-autodoc.tasks])
