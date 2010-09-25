9461cef758349df5572becd05ba9c6730793d7ad
Raynes <DiscipleRayne@gmail.com>
2010-09-25 00:13:04 -0500

    Got rid of a redundant vector.

--- src/cake_autodoc/tasks.clj
+++ src/cake_autodoc/tasks.clj
@@ -1,7 +1,7 @@
 (ns cake-autodoc.tasks
   (:use cake cake.core))
 
-(deftask autodoc []
+(deftask autodoc
   "Generates autodoc documentation for your project based on keys defined in project.clj."
   (bake (:use autodoc.autodoc) []
         (autodoc
