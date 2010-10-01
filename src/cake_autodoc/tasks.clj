3da3ee4c28f5e8d8797d9e3dac8f494ce5448157
Raynes <DiscipleRayne@gmail.com>
2010-09-30 19:31:15 -0500

    Handle options properly.

--- src/cake_autodoc/tasks.clj
+++ src/cake_autodoc/tasks.clj
@@ -3,8 +3,10 @@
 
 (deftask autodoc
   "Generates autodoc documentation for your project based on keys defined in project.clj."
+  (prn *command-line-args*)
   (bake (:use autodoc.autodoc) []
         (autodoc
          (merge
-          (select-keys *project* [:name :description :root])
-          (:autodoc *project*)))))
+          (select-keys *project* [:name :description :root :source-path])
+          (:autodoc *project*))
+         (next *command-line-args*))))
